c72d3e5fa55618d39daaddbc869e6aa19a148b71
Ash <ashm4@ash-mac.local>
2025-04-14 13:06:14 +0200

    Add email verification page and Firebase setup

--- app/page.js
+++ app/page.js
@@ -1,18 +1,41 @@
+"use client";
+import { useEffect } from "react";
 import Image from "next/image";
+import { auth } from "../firebase";
+import { applyActionCode } from "firebase/auth";
 
-export default function Home() {
+export default function EmailVerifiedPage() {
+  useEffect(() => {
+    const confirmEmail = async () => {
+      const urlParams = new URLSearchParams(window.location.search);
+      const oobCode = urlParams.get("oobCode");
+
+      if (oobCode) {
+        try {
+          await applyActionCode(auth, oobCode);
+          console.log("✅ Email verified!");
+          window.location.href = "thisway://emailVerified";
+        } catch (err) {
+          console.error("❌ Email verification failed:", err.message);
+        }
+      }
+    };
+
+    confirmEmail();
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="w-screen h-screen flex items-center justify-center bg-black">
+      <div className="flex items-center justify-center">
         <Image
-          className="dark:invert"
+          className=""
           src="/adaptive-icon.png"
           alt="ThisWay logo"
           width={180}
-          height={38}
+          height={180}
           priority
         />
-      </main>
+      </div>
     </div>
   );
 }
